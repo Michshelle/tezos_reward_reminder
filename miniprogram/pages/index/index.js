7d9c9a5bd2b8f12f28a51510908d4080a8d049c7
michshelle <shingyi.sung@gmail.com>
2021-01-16 01:58:10 +0800

    conversation interface

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,118 +1,135 @@
-//index.js
-const app = getApp()
+const app = getApp();
+var inputVal = '';
+var msgList = [];
+var windowWidth = wx.getSystemInfoSync().windowWidth;
+var windowHeight = wx.getSystemInfoSync().windowHeight;
+var keyHeight = 0;
+
+/**
+ * 初始化数据
+ */
+function initData(that) {
+  inputVal = '';
+
+  msgList = [{
+      speaker: 'server',
+      contentType: 'text',
+      content: '你好，欢迎使用Tezos烘焙奖励提醒！'
+    },
+    {
+      speaker: 'customer',
+      contentType: 'text',
+      content: '我怕是走错片场了...'
+    }
+  ]
+  that.setData({
+    msgList,
+    inputVal
+  })
+}
+
+/**
+ * 计算msg总高度
+ */
+ function calScrollHeight(that, keyHeight) {
+   var query = wx.createSelectorQuery();
+   query.select('.scrollMsg').boundingClientRect(function(rect) {
+   }).exec();
+ }
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    scrollHeight: '100vh',
+    inputBottom: 0
   },
 
-  onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    initData(this);
+    this.setData({
+      cusHeadIcon: app.globalData.userInfo.avatarUrl,
+    });
+  },
 
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 获取聚焦
+   */
+  focus: function(e) {
+    keyHeight = e.detail.height;
+    this.setData({
+      scrollHeight: (windowHeight - keyHeight) + 'px'
+    });
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1),
+      inputBottom: keyHeight + 'px'
     })
+    //计算msg高度
+    // calScrollHeight(this, keyHeight);
+
   },
 
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  },
-  
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
+  //失去聚焦(软键盘消失)
+  blur: function(e) {
+    this.setData({
+      scrollHeight: '100vh',
+      inputBottom: 0
     })
-  },
-
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = `my-image${filePath.match(/\.[^.]+?$/)[0]}`
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-      },
-      fail: e => {
-        console.error(e)
-      }
+    this.setData({
+      toView: 'msg-' + (msgList.length - 1)
     })
+
   },
+
+  /**
+   * 发送点击监听
+   */
+  sendClick: function(e) {
+    msgList.push({
+      speaker: 'customer',
+      contentType: 'text',
+      content: e.detail.value
+    })
+    inputVal = '';
+    this.setData({
+      msgList,
+      inputVal
+    });
+
+
+  },
+
+  /**
+   * 退回上一页
+   */
+  toBackClick: function() {
+    wx.navigateBack({})
+  }
 
 })
--- miniprogram/pages/openapi/callback/callback.js
+++ miniprogram/pages/openapi/callback/callback.js
@@ -9,6 +9,9 @@ Page({
 
   },
 
+  //进入小程序推送的第一条信息：你好，欢迎使用Tezos烘焙奖励提醒服务！
+  
+
   onCustomerServiceButtonClick(e) {
     console.log(e)
   },
