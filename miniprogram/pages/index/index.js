55f8ab82f29f80ae4453797ef92298ee0bd15898
michshelle <shingyi.sung@gmail.com>
2021-01-16 03:01:08 +0800

    conversation interface done

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -15,11 +15,6 @@ function initData(that) {
       speaker: 'server',
       contentType: 'text',
       content: '你好，欢迎使用Tezos烘焙奖励提醒！'
-    },
-    {
-      speaker: 'customer',
-      contentType: 'text',
-      content: '我怕是走错片场了...'
     }
   ]
   that.setData({
@@ -51,10 +46,33 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+    return
+    }
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
     initData(this);
     this.setData({
-      cusHeadIcon: app.globalData.userInfo.avatarUrl,
+
     });
+
   },
 
   /**
